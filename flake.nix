71bf8234ced8926f59738737234d0405b6706ed2
Nathan Regner <nathanregner@gmail.com>
2024-10-26 21:44:12 -0600

    build: update inputs

--- flake.nix
+++ flake.nix
@@ -6,10 +6,8 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     # rust
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
