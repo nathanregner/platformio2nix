4c47246e65f3ec240aa7841353f64619e4c2572e
Nathan Regner <nathanregner@gmail.com>
2024-10-26 22:28:23 -0600

    fix: external-deps example

--- examples/external-deps/package.nix
+++ examples/external-deps/package.nix
@@ -1,5 +1,6 @@
 {
   gnumake,
+  libarchive,
   makePlatformIOSetupHook,
   platformio,
   stdenv,
@@ -11,7 +12,14 @@ let
   setupHook = makePlatformIOSetupHook {
     lockfile = ./platformio2nix.lock;
     overrides = (
-      final: prev: { SlowSoftWire = prev.SlowSoftWire.overrideAttrs { dontUnpack = true; }; }
+      final: prev: {
+        SlowSoftWire = prev.SlowSoftWire.overrideAttrs {
+          nativeBuildInputs = [ libarchive ];
+          unpackPhase = ''
+            bsdtar xf $src --strip-components=1
+          '';
+        };
+      }
     );
   };
 in
